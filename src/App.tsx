8666d016825fb3f4658feb01499183de39292d1a
Sergio <sergiopdjr@hotmail.com>
2025-01-20 01:05:18 -0300

    alterando css modules para styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
-
-import './global.css'
+import EstiloGlobal, { Container } from './styles'
+import temaPrincipal from './Theme/tema'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaPrincipal}>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
---
+++ src/Theme/tema.ts
@@ -0,0 +1,7 @@
+const temaPrincipal = {
+  corPrincipal: '#a7727d',
+  corSecundaria: '#f9f5e7',
+  CorTextoHero: '#eee'
+}
+
+export default temaPrincipal
